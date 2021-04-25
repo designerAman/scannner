ce420c15a8f93a28ce112cfb93fd14bf82cfb83a
Aman2601 <debugger.aman@gmail.com>
2021-04-26 01:48:40 +0530

    test 4

--- JS/script.js
+++ JS/script.js
@@ -1,133 +1,153 @@
-// // The buttons to start & stop stream and to capture the image
-// var btnStart = document.getElementById("btn-start");
-// var btnStop = document.getElementById("btn-stop");
+// The buttons to start stream and to capture the image
+var btnStart = document.getElementById("btn-start");
 // var btnCapture = document.getElementById("btn-capture");
 
-// // The stream & capture
-// var stream = document.getElementById("stream");
-// var capture = document.getElementById("capture");
-// var snapshot = document.getElementById("snapshot");
+// The stream & capture
+var stream = document.getElementById("stream");
+var capture = document.getElementById("capture");
+var snapshot = document.getElementById("snapshot");
 
-// // The video stream
-// var cameraStream = null;
+// The video stream
+var cameraStream = null;
 
-// // Attach listeners
-// btnStart.addEventListener("click", startStreaming);
-// btnStop.addEventListener("click", stopStreaming);
+// Attach listeners
+btnStart.addEventListener("click", startStreaming);
 
-// // Start Streaming
-// function startStreaming() {
+// Start Streaming
+function startStreaming() {
+  var mediaSupport = 'mediaDevices' in navigator;
 
-//   var mediaSupport = 'mediaDevices' in navigator;
+  if (mediaSupport && null == cameraStream) {
 
-//   if (mediaSupport && null == cameraStream) {
+    const constraints = {
+      video: {
+        facingMode: { exact: "environment" }
+      },
+      // video:true,s
+      audio: false,
+    }
 
-//     const constraints = {
-//       // video: {
-//       //   facingMode: { exact: "environment" }
-//       // },
-//       video:true,
-//       audio: false,
-//     }
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(function (mediaStream) {
 
-//     navigator.mediaDevices.getUserMedia(constraints)
-//       .then(function (mediaStream) {
+        cameraStream = mediaStream;
 
-//         cameraStream = mediaStream;
+        stream.srcObject = mediaStream;
 
-//         stream.srcObject = mediaStream;
+        stream.play();
 
-//         stream.play();
-//       })
-//       .catch(function (err) {
-//         console.log("Unable to access camera: " + err);
-//       });
-//   }
-//   else {
+        setTimeout(() => {
+          captureSnapshot();
+        }, 1500);
+      })
+      .catch(function (err) {
+        console.log("Unable to access camera: " + err);
+      });
+  }
+  else {
 
-//     alert('Your browser does not support media devices.');
+    alert('Your browser does not support media devices.');
 
-//     return;
-//   }
-// }
-
-// // Stop Streaming
-// function stopStreaming() {
-
-//   if (null != cameraStream) {
-
-//     var track = cameraStream.getTracks()[0];
-
-//     track.stop();
-//     stream.load();
-
-//     cameraStream = null;
-//   }
-// }
+    return;
+  }
+}
 
 // btnCapture.addEventListener("click", captureSnapshot);
+let imageToScan;
 
-// function captureSnapshot() {
+function captureSnapshot() {
 
-//   if (null != cameraStream) {
+  if (null != cameraStream) {
 
-//     var ctx = capture.getContext('2d');
-//     var img = new Image();
+    var ctx = capture.getContext('2d');
+    var img = new Image();
 
-//     ctx.drawImage(stream, 0, 0, capture.width, capture.height);
+    ctx.drawImage(stream, 0, 0, capture.width, capture.height);
 
-//     img.src = capture.toDataURL("image/png");
-//     img.width = 240;
+    img.src = capture.toDataURL("image/png");
+    img.width = 240;
 
-//     snapshot.innerHTML = '';
+    snapshot.innerHTML = '';
 
-//     snapshot.appendChild(img);
+    snapshot.appendChild(img);
 
-//     console.log(img.src);
-//     console.log(snapshot);
-//   }
-// }
+    console.log(img.src);
+    console.log(snapshot);
 
-// function dataURItoBlob(dataURI) {
+    imageToScan = dataURItoBlob(img.src);
 
-//   var byteString = atob(dataURI.split(',')[1]);
-//   var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    scanFile(imageToScan);
+  }
+}
 
-//   var buffer = new ArrayBuffer(byteString.length);
-//   var data = new DataView(buffer);
+function dataURItoBlob(dataURI) {
 
-//   for (var i = 0; i < byteString.length; i++) {
+  var byteString = atob(dataURI.split(',')[1]);
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-//     data.setUint8(i, byteString.charCodeAt(i));
-//   }
+  var buffer = new ArrayBuffer(byteString.length);
+  var data = new DataView(buffer);
 
-//   return new Blob([buffer], { type: mimeString });
-// }
+  for (var i = 0; i < byteString.length; i++) {
 
-// function initializeScanner() {
-//   startStreaming();
+    data.setUint8(i, byteString.charCodeAt(i));
+  }
 
-// }
+  return new Blob([buffer], { type: mimeString });
+}
+
+function initializeScanner() {
+  startStreaming();
+
+  // setTimeout(() => {
+  //   cameraStream();
+  // }, 1500);
+}
 
 const html5QrCode = new Html5Qrcode(/* element id */ "reader");
 
-// File based scanning
-const fileinput = document.getElementById('qr-input-file');
-fileinput.addEventListener('change', e => {
-  if (e.target.files.length == 0) {
-    // No file selected, ignore 
-    return;
-  }
+function ScanCode(file) {
+  html5QrCode.scanFile(file, /* showImage= */true)
+  .then(qrCodeMessage => {
+    // success, use qrCodeMessage
+    console.log(qrCodeMessage);
+    alert(qrCodeMessage);
+  })
+  .catch(err => {
+    // failure, handle it.
+    console.log(`Error scanning file. Reason: ${err}`)
+  });
+}
 
-  // Use the first item in the list
-  const imageFile = e.target.files[0];
-  html5QrCode.scanFile(imageFile, /* showImage= */true)
-    .then(qrCodeMessage => {
-      // success, use qrCodeMessage
-      console.log(qrCodeMessage);
-    })
-    .catch(err => {
-      // failure, handle it.
-      console.log(`Error scanning file. Reason: ${err}`)
-    });
-});
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// File based scanning
+// const fileinput = document.getElementById('qr-input-file');
+// fileinput.addEventListener('change', e => {
+//   if (e.target.files.length == 0) {
+//     // No file selected, ignore 
+//     return;
+//   }
+
+//   // Use the first item in the list
+//   console.log(e);
+//   const imageFile = e.target.files[0];
+//   console.log(imageFile);
+//   html5QrCode.scanFile(imageFile, /* showImage= */true)
+//     .then(qrCodeMessage => {
+//       // success, use qrCodeMessage
+//       console.log(qrCodeMessage);
+//     })
+//     .catch(err => {
+//       // failure, handle it.
+//       console.log(`Error scanning file. Reason: ${err}`)
+//     });
+// });
