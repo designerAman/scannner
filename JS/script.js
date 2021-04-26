07668da59b2b4d39ad9b3601095c010674e6c68e
Aman2601 <debugger.aman@gmail.com>
2021-04-27 00:37:40 +0530

    test 3

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,7 @@ btnStop.addEventListener("click", stopStreaming);
 var cameraStream = null;
 
 function startStreaming() {
+  alert('start');
   const mediaSupport = 'mediaDevices' in navigator;
 
   if (mediaSupport && null == cameraStream) {
@@ -45,24 +46,26 @@ function startStreaming() {
 
 function stopStreaming() {
 
-	if (null != cameraStream) {
+  if (null != cameraStream) {
 
-		var track = cameraStream.getTracks()[0];
+    var track = cameraStream.getTracks()[0];
 
-		track.stop();
-		stream.load();
+    track.stop();
+    stream.load();
 
-		cameraStream = null;
-	}
+    cameraStream = null;
+  }
 }
 
 async function captureStream() {
-  if(cameraStream !== null) {
+  alert('start capturing');
+
+  if (cameraStream !== null) {
     const ctx = capture.getContext('2d');
     const img = new Image();
 
     ctx.drawImage(stream, 0, 0, capture.width, capture.height);
-    
+
     img.src = capture.toDataURL("image/png");
     img.width = 320;
 
@@ -75,36 +78,41 @@ async function captureStream() {
     console.log(QRImage);
 
     ScanCode(QRImage);
+
+    alert('completed scaning');
   }
 }
 
 function dataURLtoFile(dataurl, filename) {
+  alert('start converting');
+
   var arr = dataurl.split(','),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), 
-      n = bstr.length, 
-      u8arr = new Uint8Array(n);
-      
-  while(n--){
-      u8arr[n] = bstr.charCodeAt(n);
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
   }
-  
-  return new File([u8arr], filename, {type:mime});
+
+  return new File([u8arr], filename, { type: mime });
 }
 
 const html5QrCode = new Html5Qrcode(/* element id */ "reader");
 
 function ScanCode(file) {
+  alert('start scanning');
   html5QrCode.scanFile(file, /* showImage= */true)
-  .then(qrCodeMessage => {
-    // success, use qrCodeMessage
-    console.log(qrCodeMessage);
-    alert(qrCodeMessage);
-  })
-  .catch(err => {
-    // failure, handle it.
-    console.log(`Error scanning file. Reason: ${err}`)
-    alert(err);
-    alert(JSON.stringify(err));
-  });
+    .then(qrCodeMessage => {
+      // success, use qrCodeMessage
+      console.log(qrCodeMessage);
+      alert(qrCodeMessage);
+    })
+    .catch(err => {
+      // failure, handle it.
+      console.log(`Error scanning file. Reason: ${err}`)
+      alert(err);
+      alert(JSON.stringify(err));
+    });
 }
